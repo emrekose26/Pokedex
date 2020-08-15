344c3a8c04ddd7a0dadd36b38a6e56e35096b15e
Emre Köse <e.kose26@gmail.com>
2020-08-15 12:09:54 +0300

    Model classes created

---
+++ app/src/main/java/com/emrekose/pokedex/model/Pokemon.kt
@@ -0,0 +1,18 @@
+package com.emrekose.pokedex.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Pokemon(
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "url")
+    val url: String
+) {
+    fun getPokemonImageUrl(): String {
+        val index = url.split("/".toRegex()).dropLast(1).last()
+        return "https://pokeres.bastionbot.org/images/pokemon/$index.png"
+    }
+}
---
+++ app/src/main/java/com/emrekose/pokedex/model/PokemonResponse.kt
@@ -0,0 +1,17 @@
+package com.emrekose.pokedex.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PokemonResponse(
+    @Json(name = "count")
+    val count: Int,
+    @Json(name = "next")
+    val next: String,
+    @Json(name = "previous")
+    val previous: Any,
+    @Json(name = "results")
+    val results: List<Pokemon>
+)
